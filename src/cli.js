c46cfa06fc037c838603602db9aa4e33a4b26bf1
Bruno Basto <bruno.basto@liferay.com>
2015-05-31 15:15:04 -0300

    Lets be consistent

--- src/cli.js
+++ src/cli.js
@@ -7,8 +7,8 @@ var WaterMarker = require('./index.js');
 var argv = rc(
 	'node-watermark', {}, optimist
 	.usage('Usage: $0 video_file -w watermark_file [options]')
-	.alias('o', 'output').describe('o', 'The path of the output video file').boolean('a')
-	.alias('v', 'version').describe('version', 'Prints current version').boolean('boolean')
+	.alias('o', 'output').describe('o', 'The path of the output video file.').boolean('a')
+	.alias('v', 'version').describe('version', 'Prints current version.').boolean('boolean')
 	.alias('w', 'watermark').describe('w', '[Required] The watermark image path.').boolean('a')
 	.argv);
 
